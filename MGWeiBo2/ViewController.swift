71c1c11bfd0affcb289c1a6ee449b830f8cc1f2b
angmu <anglimu@163.com>
2016-12-18 21:27:03 +0800

    创建工程

---
+++ MGWeiBo2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MGWeiBo2
+//
+//  Created by 穆良 on 2016/12/18.
+//  Copyright © 2016年 穆良. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
