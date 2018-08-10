f9dc95e36dec73b86931bd71df5ca719f72bc5eb
muliang001 <anglimu@163.com>
2018-08-10 22:22:50 +0800

    删除多余swift文件

--- MGWeiBo2/Classes/Main(主要)/MainViewController.swift
+++ MGWeiBo2/Classes/Main(主要)/MainViewController.swift
@@ -16,24 +16,23 @@ class MainViewController: UITabBarController {
         // iOS7 文字图片 都设置
         tabBar.tintColor = UIColor.orange
         addChildViewControllers()
-        
-        // 测试命名空间
-        transStringToClass("HomeViewController")
     }
     
     /// 添加所有子控制器
     func addChildViewControllers() {
-        addChildViewController(HomeViewController(), title: "首页", imageName: "tabbar_home")
-        addChildViewController(MessageViewController(), title: "消息", imageName: "tabbar_message_center")
-        addChildViewController(DiscoverViewController(), title: "发现", imageName: "tabbar_discover")
-        addChildViewController(ProfileViewController(), title: "我", imageName: "tabbar_profile")
+        addChildViewController("HomeViewController", title: "首页", imageName: "tabbar_home")
+        addChildViewController("MessageViewController", title: "消息", imageName: "tabbar_message_center")
+        addChildViewController("DiscoverViewController", title: "发现", imageName: "tabbar_discover")
+        addChildViewController("ProfileViewController", title: "我", imageName: "tabbar_profile")
     }
     
-    /// 添加子控制器
-    /// 重载系统方法，扩充
-    private func addChildViewController(_ childVc: UIViewController, title : String, imageName : String) {
+    /// 添加子控制器，重载系统方法，扩充
+    private func addChildViewController(_ childVcStr: String, title : String, imageName : String) {
         
-        MGLog(childVc)
+        guard let childVc = transStringToClass(childVcStr) else {
+            MGLog("\(childVcStr)类名称有错误")
+            return
+        }
         
         // 设置chileVc属性
         childVc.title = title;
@@ -46,45 +45,30 @@ class MainViewController: UITabBarController {
         addChildViewController(childNav)
     }
     
-    /// 查看命名空间
-    func transStringToClass(_ name:String) {
-        
-        var testme :String? = "Hello world"
-        MGLog("测试呢----\(testme!)")
+    
+    /// 字符串创建类型
+    func transStringToClass(_ name:String) -> UIViewController? {
         // 1.拿到命名空间
-        let testName: Any? = Bundle.main.infoDictionary!["CFBundleExecutable"]
-        MGLog(testName)
-        
-        if let testName2 = Bundle.main.infoDictionary!["CFBundleExecutable"] {
-            MGLog(testName2)
-        }
-        
         guard let nameSpace = Bundle.main.infoDictionary!["CFBundleExecutable"] as? String else {
             MGLog("获取不到命名空间")
-            return
+            return nil
         }
 
         // 2.转成类型Class -> AnyClass
         guard let className = NSClassFromString(nameSpace + "." + name) else {
             MGLog("获取不到类名")
-            return
+            return nil
         }
 
         // 3.AnyClass转成对应控制器的类型
         guard let typeClass = className as? UIViewController.Type else {
             MGLog("没有获取到对应控制器类型")
-            return
+            return nil
         }
         
-        
         // 4.创建对应的控制器对象
-        let childVc = typeClass.init()
-        MGLog(childVc)
-        
-        MGLog("nameSpace=\(nameSpace)---className=\(className)---typeClass=\(typeClass)")
+        return typeClass.init()
     }
-    
-
 }
 
 
