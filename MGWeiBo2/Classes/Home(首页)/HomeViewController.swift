64e2deb19995fb7f81a87af7dad54b4defd6614f
MGBook <liangmu.gdut@gmail.com>
2020-09-14 11:09:03 +0800

    自定义转场动画—弹出

--- MGWeiBo2/Classes/Home(首页)/HomeViewController.swift
+++ MGWeiBo2/Classes/Home(首页)/HomeViewController.swift
@@ -52,7 +52,50 @@ extension HomeViewController {
 
 //MARK:- 代理相关
 extension HomeViewController: UIViewControllerTransitioningDelegate {
+    // 自定义弹出view的尺寸
     func presentationController(forPresented presented: UIViewController, presenting: UIViewController?, source: UIViewController) -> UIPresentationController? {
         return MGPresentationController(presentedViewController: presented, presenting: presenting)
     }
+    
+    // 自定义弹出动画
+    func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        return self
+    }
+    
+    // 自定义消失动画
+//    func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+//        return self
+//    }
+    
+    
+}
+
+//MARK:- 转场动画相关
+extension HomeViewController: UIViewControllerAnimatedTransitioning {
+    func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+        return 0.5
+    }
+    
+    func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+        // 拿到弹出的view
+        let presetedView = transitionContext.view(forKey: .to)!
+        
+        // 将弹出view添加到containView中
+        transitionContext.containerView.addSubview(presetedView)
+        
+        // 执行动画
+        presetedView.transform = CGAffineTransform(scaleX: 1.0, y: 0)
+        // 动画从中间开始？–≥ 设置锚点
+        presetedView.layer.anchorPoint = CGPoint(x: 0.5, y: 0)
+        UIView .animate(withDuration: transitionDuration(using: transitionContext), animations: {
+            presetedView.transform = CGAffineTransform.identity
+        }) { (_)->() in
+            // 必须告诉转场上下文，动画已经结束
+            transitionContext.completeTransition(true)
+        }
+        
+        
+    }
+    
+    
 }
