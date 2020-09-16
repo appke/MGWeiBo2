902392a16abba29ae0becb6ff62c442cfecd1473
MGBook <liangmu.gdut@gmail.com>
2020-09-16 20:46:13 +0800

    调整工程结构

---
+++ MGWeiBo2/Classes/Tools(工具)/NetworkTools.swift
@@ -0,0 +1,31 @@
+//
+//  NetworkTools.swift
+//  MGWeiBo2
+//
+//  Created by MGBook on 2020/9/16.
+//  Copyright © 2020 穆良. All rights reserved.
+//
+
+import AFNetworking
+
+class NetworkTools: AFHTTPSessionManager {
+    
+    static let shared = NetworkTools()
+
+}
+
+//MARK:- 封装请求方法
+
+//MARK:- 请求AccessToken
+extension NetworkTools {
+}
+
+//MARK:- 请求用户信息
+extension NetworkTools {
+}
+
+
+//MARK:- 请求首页数据
+extension NetworkTools {
+    
+}
